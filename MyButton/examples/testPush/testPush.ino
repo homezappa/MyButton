e4bb52365b4582c9521a93e20c46e5492af75bcc
homezappa <andrea@casazappa.it>
2020-02-03 18:22:04 +0100

    Release 1.1
    Monitor only via Serial, without delays

--- MyButton/examples/testPush/testPush.ino
+++ MyButton/examples/testPush/testPush.ino
@@ -6,37 +6,31 @@
 // MyButton object. Note: I have an NormallyClosed button, so I wired it from ground and pin 2, 
 // and then INPUT_PULLUP is needed
 
-MyButton  btn(PIN_BUTTON, INPUT_PULLUP); 
+MyButton  btn; 
 
 void setup() {
-  // we'll use the led builtin
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-
   Serial.begin(9600);
-  Serial.println("Blinking Led");
+  Serial.println("Test Push");
+  
+  btn.begin(PIN_BUTTON, INPUT_PULLUP); 	
 
-  // set long delay (defaults at 2000 msec)
-  btn.setLongPushDelay(2500);
+// set long delay (defaults at 2000 msec)
+  btn.setLongPushDelay(3000);
   // set push delay (defaults at 80 msec)
-  btn.setPushDelay(60);
+  btn.setPushDelay(100);
 }
 
 void loop() {
   // every loop I read the button
   btn.read();
+  if (btn.wasLongPushed()==true) {
+    Serial.println("WAS Long Pushed");
+  } 
   if (btn.wasPushed()==true) {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
-    delay(500);                        // wait for half a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
-  } else if (btn.wasLongPushed()==true) {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
-    delay(2000);                       // wait for two seconds
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
-  } else if (btn.isPushed()==true) {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
-    delay(50);                        // wait for 50 milliseconds only
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
+    Serial.println("WAS Pushed");
+  } 
+  if (btn.isPushed()==true) {
+    Serial.println("IS Pushed");
   }
-  delay(50);
+  delay(5);
 }
